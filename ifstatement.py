db7550d2c525e0f92f392681f88576bba1808abf
nezaapriani <incesnezaapriani@gmail.com>
2026-04-20 19:22:18 +0800

    Add files via upload

---
+++ ifstatement.py
@@ -0,0 +1,15 @@
+#percabangan
+#1. if nya
+#2. kondisi
+#3. aksi
+
+nama = input("masukan nama anda :")
+if nama=="edy" : #kondidi pertama
+    print("selamat datang edy") #aksi true pertama
+elif nama=="syahdik" : #kondidi kedua
+     print("selamat datang syahdik") #aksi true pertama
+elif nama=="syahdik" : #kondidi ketiga
+     print("selamat datang syahdik") #aksi true pertama
+else : 
+    print("anda bukan dosen")
+print("program selesai")

cd4a918a8d8e7d1dc23247acdf6aa7b8ac0adf07
nezaapriani <incesnezaapriani@gmail.com>
2026-04-22 07:01:42 +0800

    Add files via upload

---
+++ catatanpert3.py
@@ -0,0 +1,15 @@
+#perulangan while
+#kondisi :
+ # aksi
+ 
+ 
+ 
+angka = 0
+print("nilai angka : (angka}")
+
+while angka <= 4 :
+       angka += 1
+       print(f"nilai angka : {angka}")
+       print("pak edy ganteng")
+       
+print("program selesai")
